fa82b2b145e0b58d4b830d4876d2e4e4bc7c27b0
Ashwin K S <ashwin.k.sri@gmail.com>
2020-04-02 17:21:32 +0530

    Initial Commit

---
+++ forum/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Question(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	question = models.CharField(max_length=200, blank=True)
+	timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+	def __str__(self):
+		return self.question
+
+class Answer(models.Model):
+	answer = models.CharField(max_length=500, blank=True)
+	timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	upvotes = models.IntegerField(blank=True)
+
+	def __str__(self):
+		return self.answer
+
+class Upvote(models.Model):
+	reader = models.ForeignKey(User, on_delete=models.CASCADE)
+	answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
